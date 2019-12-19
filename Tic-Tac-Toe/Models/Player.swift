b301eb937c5ed9bde6968c163fbddd7a0f25ab08
Anthony_torres44@icloud.com <anthony_torres44@icloud.com>
2019-12-19 12:15:10 -0700

    Finished ViewController

--- Tic-Tac-Toe/Models/Player.swift
+++ Tic-Tac-Toe/Models/Player.swift
@@ -9,15 +9,19 @@
 import UIKit
 
 class Player {
-    var x: UIImage
-    var o: UIImage
     var wins: Int
-    var turn: Bool
+    var locations: [Int]
+    var name: String
     
-    init(x: UIImage, o: UIImage, wins: Int, turn: Bool) {
-        self.x = x
-        self.o = o
+    init(wins: Int = 0, locations: [Int] = [], name: String) {
         self.wins = wins
-        self.turn = turn
+        self.locations = locations
+        self.name = name
+    }
+}
+
+extension Player: Equatable {
+    static func == (lhs: Player, rhs: Player) -> Bool {
+        return lhs.name == rhs.name
     }
 }
